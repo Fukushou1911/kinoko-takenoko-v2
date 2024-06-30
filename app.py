e36a502db25a5e95ad8a0e9e5f6e0c1a8f1de1a9
Fukushou1911 <s1f102401427@iniad.org>
2024-06-30 23:09:51 +0900

    リンク検出機能を実装

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import re
 app = Flask(__name__)
 
 kinoko_count = 3
@@ -20,9 +21,18 @@ def answer():
     if len(messages) > 3:
         messages = messages[-3:]
     print(request.form)
+    message_html = ''
+    for i in range(len(messages)):
+        message = messages[i]
+        message = re.sub(r'&', r'&amp;', message)
+        message = re.sub(r'<', r'&1t;', message)
+        message = re.sub(r'>', r'&gt;', message)
+        message = re.sub(r'\*(.+)\*', r'<strong>\1<strong>', message)
+        message = re.sub(r'(\d{2,3})-\d+-\d+', r'\1-****-****', message)
+        message = re.sub(r'https?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', r'<a href="\g<0>">\g<0></a>', message)
+        message_html += '<div class="alert {1}" role="alert">{0}</div>\n'.format(message, 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5')
+        return render_template('vote.html', **vars())
     kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
     takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
-    return render_template('vote.html', **vars())
-
 if __name__ == '__main__':
     app.run(debug=True)
